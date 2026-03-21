00d4ad01b3b50f583e5bb93c27c8cb1ccea5bc82
Iain Millar <iain.millar@thetin.net>
2026-03-21 21:52:11 +0000

    Fix WebView scaling and grey bar on FireTV
    - setUseWideViewPort + setLoadWithOverviewMode: scale 1024px page to fill TV screen
    - setBackgroundColor to match page background (eliminates grey flash)
    - Add LAYOUT_FULLSCREEN flags to properly hide system bars

--- app/src/main/java/net/thetin/hearth/MainActivity.java
+++ app/src/main/java/net/thetin/hearth/MainActivity.java
@@ -1,6 +1,7 @@
 package net.thetin.hearth;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
@@ -21,15 +22,21 @@ public class MainActivity extends Activity {
         getWindow().getDecorView().setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_FULLSCREEN |
                 View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
-                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
         );
 
         WebView webView = new WebView(this);
+        webView.setBackgroundColor(Color.parseColor("#0d0906"));
         setContentView(webView);
 
         WebSettings settings = webView.getSettings();
         settings.setJavaScriptEnabled(true);
         settings.setDomStorageEnabled(true);
+        settings.setUseWideViewPort(true);
+        settings.setLoadWithOverviewMode(true);
 
         webView.setWebViewClient(new WebViewClient());
         webView.loadUrl(DISPLAY_URL);
